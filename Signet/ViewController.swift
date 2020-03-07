29ca5e93295d965733a485b51a4443fd7b6f4fa7
Riku <joker_avan@icloud.com>
2020-03-07 14:42:02 +0900

    Initial Commit

---
+++ Signet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Signet
+//
+//  Created by Riku on 2020/03/07.
+//  Copyright © 2020 Riku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
